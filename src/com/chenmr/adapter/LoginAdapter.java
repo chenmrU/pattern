a38012f03f4d20cf316a0ff81edc895fa2da45ec
immary <1069865896@qq.com>
2018-04-12 23:18:27 +0800

    装饰器模式

---
+++ src/com/chenmr/adapter/LoginAdapter.java
@@ -0,0 +1,12 @@
+package com.chenmr.adapter;
+
+/**
+ * Created by Chenmr on 2018/4/12/012.
+ */
+public class LoginAdapter extends Login{
+
+    public void login(){
+        super.login();
+    }
+
+}
