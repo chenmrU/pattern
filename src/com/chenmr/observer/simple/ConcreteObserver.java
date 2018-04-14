d0c5974d5bdd52039aec0b04dddd8f4143fd56a5
immary <1069865896@qq.com>
2018-04-14 13:59:55 +0800

    观察者模式

---
+++ src/com/chenmr/observer/simple/ConcreteObserver.java
@@ -0,0 +1,11 @@
+package com.chenmr.observer.simple;
+
+/**
+ * 具体观察者
+ */
+public class ConcreteObserver implements Observer{
+    //实现响应方法
+    public void update(){
+        //具体响应代码
+    }
+}
---
+++ src/com/chenmr/observer/simple/Observer.java
@@ -0,0 +1,9 @@
+package com.chenmr.observer.simple;
+
+/**
+ * 抽象观察者
+ */
+public interface Observer {
+    //声明响应方法
+    void update();
+}
